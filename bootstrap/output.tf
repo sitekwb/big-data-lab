1ce7539ff7cd5f9637ab2c15818993ff2e4e3225
Marek Wieiwórka <marek.wiewiorka@gmail.com>
2024-11-30 14:36:14 +0100

    Fixing refs

--- bootstrap/output.tf
+++ bootstrap/output.tf
@@ -3,11 +3,11 @@ output "project_name" {
   description = "Project identifier"
 }
 output "terraform_state_bucket" {
-  value       = trimprefix(google_storage_bucket.tbd-state-bucket.url, "gs://")
+  value       = trimprefix(google_storage_bucket.state-bucket.url, "gs://")
   description = "Terraform state bucket"
 }
 
 output "terraform_service_account" {
-  value       = google_service_account.tbd-terraform.email
+  value       = google_service_account.terraform.email
   description = "Terraform service account"
 }
