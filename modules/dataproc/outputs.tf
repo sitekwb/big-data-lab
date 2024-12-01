7b510c6926308751a6a03bc2c53e7f94ca2692e1
Marek Wieiwórka <marek.wiewiorka@gmail.com>
2024-12-01 12:55:45 +0100

    Repo refactor

--- modules/dataproc/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "dataproc_cluster_name" {
-  description = "Dataproc cluster name"
-  value       = google_dataproc_cluster.dataproc-cluster.name
-}
--- modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "network" {
-  value       = module.vpc.network
-  description = "VPC id"
-}
-
-output "subnets" {
-  value       = module.vpc.subnets
-  description = "VPC subnets map"
-}
